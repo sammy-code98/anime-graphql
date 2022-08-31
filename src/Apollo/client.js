241ae328f0b4bf03ce0c865fb5297fd19ac55ee5
sammy-code98 <sammysmart98@yahoo.com>
2022-08-31 23:07:48 +0100

    make query

--- src/Apollo/client.js
+++ src/Apollo/client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://graphql.anilist.co/',
+  uri: 'https://graphql.anilist.co',
   cache: new InMemoryCache(),
 });
--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './Apollo/client';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Anime from './component/Anime';
 
 function App() {
   return (
@@ -21,21 +12,7 @@ function App() {
         <Box textAlign="center" fontSize="xl">
           <Grid minH="100vh" p={3}>
             <ColorModeSwitcher justifySelf="flex-end" />
-            <VStack spacing={8}>
-              <Logo h="40vmin" pointerEvents="none" />
-              <Text>
-                Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-              </Text>
-              <Link
-                color="teal.500"
-                href="https://chakra-ui.com"
-                fontSize="2xl"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn Chakra
-              </Link>
-            </VStack>
+            <Anime />
           </Grid>
         </Box>
       </ChakraProvider>
