f805342d82455129601f765776c7f7820c58bcb3
sammy-code98 <sammysmart98@yahoo.com>
2022-08-31 22:31:46 +0100

    install and configure apollo-client

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './Apollo/client';
 import {
   ChakraProvider,
   Box,
@@ -14,28 +16,30 @@ import { Logo } from './Logo';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <Box textAlign="center" fontSize="xl">
+          <Grid minH="100vh" p={3}>
+            <ColorModeSwitcher justifySelf="flex-end" />
+            <VStack spacing={8}>
+              <Logo h="40vmin" pointerEvents="none" />
+              <Text>
+                Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+              </Text>
+              <Link
+                color="teal.500"
+                href="https://chakra-ui.com"
+                fontSize="2xl"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn Chakra
+              </Link>
+            </VStack>
+          </Grid>
+        </Box>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
